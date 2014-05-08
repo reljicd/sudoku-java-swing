dc567949b58464a8a043fff896df58cc50bdacbb
Dusan Reljic <reljicd@gmail.com>
2014-05-08 15:28:13 +0200

    Menu is working, Undo and Save still not implemented

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,55 +1,41 @@
 package view;
 
-import java.util.Observable;
-import java.util.Observer;
-
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 import controller.MenuController;
-import model.UpdateAction;
 
-public class Menu implements Observer{
-	
-    JMenuBar menuBar;
-    JMenu menu;
-    JMenuItem newGameItem, saveGameItem, undoItem, exitItem;
- 
-    public JMenuBar createMenuBar() {
- 
-        //Create the menu bar.
-        menuBar = new JMenuBar();
- 
-        //Build the first menu.
-        menu = new JMenu("Options");
-        menuBar.add(menu);
- 
-        //a group of JMenuItems
-        newGameItem = new JMenuItem("New Game");
-        menu.add(newGameItem);
-        
-        saveGameItem = new JMenuItem("Save Game");
-        menu.add(saveGameItem);
+public class Menu extends JMenuBar {
 
-        undoItem = new JMenuItem("Undo");
-        menu.add(undoItem);
-        
-        //a group of JMenuItems
-        exitItem = new JMenuItem("Exit");
-        menuBar.add(exitItem);
- 
-        return menuBar;
-    }
+	JMenu menu;
+	JMenuItem newGameItem, saveGameItem, undoItem, exitItem;
 
-	@Override
-	public void update(Observable o, Object arg) {
-        switch ((UpdateAction)arg) {
-            case NEW_GAME:
-        }		
+	public Menu() {
+
+		// Create the menu bar.
+		super();
+
+		// Build the first menu.
+		menu = new JMenu("Options");
+		add(menu);
+
+		// a group of JMenuItems
+		newGameItem = new JMenuItem("New Game");
+		menu.add(newGameItem);
+
+		saveGameItem = new JMenuItem("Save Game");
+		menu.add(saveGameItem);
+
+		undoItem = new JMenuItem("Undo");
+		menu.add(undoItem);
+
+		// a group of JMenuItems
+		exitItem = new JMenuItem("Exit");
+		add(exitItem);
 	}
-	
-	public void setController(MenuController menuController){
+
+	public void setController(MenuController menuController) {
 		newGameItem.addActionListener(menuController);
 		saveGameItem.addActionListener(menuController);
 		undoItem.addActionListener(menuController);
